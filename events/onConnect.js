85029e75e4e54cebbb50fc0132f6585a518e480d
Chiara <chiara.biancacci@instathings.io>
2020-04-17 14:48:34 +0200

    dynamic mqtt topic

--- events/onConnect.js
+++ events/onConnect.js
@@ -2,9 +2,22 @@ const onMessage = require('./onMessage');
 
 module.exports = function onConnectFn(client, config) {
   return function onConnect() {
+    let protocol;
     this.log('Connected');
-    const { friendly } = config;
-    const topic = `zigbee2mqtt/${friendly}`;
+    const splitted = config.friendly.split('|');
+    const protocolId = splitted[0];
+    const friendly = splitted[1];
+    switch (protocolId) {
+      case 'modbus': {
+        protocol = 'modbus';
+        break;
+      }
+      case 'vRy6GTde': {
+        protocol = 'zigbee';
+        break;
+      }
+    }
+    const topic = `${protocol}2mqtt/${friendly}`;
     client.on('message', onMessage.bind(this));
     client.subscribe(topic, () => {
       this.log('Subscribed');
